80a565328774a2f50ee969d6efd5eefacb58ede6
hugo <hugolimaxavier@gmail.com>
2023-07-17 15:50:56 -0300

    Organizando GET  de POST em files  diferentes

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,39 +1,7 @@
 describe('Teste de backend - Qacoders', () => {
     beforeEach(() => {
-        cy.loginWithToken();
-    });
-    it('Requisitar logins', () => {
-        cy.request({
-            method: 'GET',
-            url: `${Cypress.env("baseUrl")}/user`,
-            failOnStatusCode: false,
-            headers: {
-                Authorization: `${Cypress.env("authorizationToken")}`
-            }
-        }).then((response) => {
-            expect(response.status).to.eq(200)
-        })
-
+        cy.loginWithToken()
     })
-
-    it.only('Requisitar logins sem token', () => {
-        cy.request({
-            method: 'GET',
-            url: `${Cypress.env("baseUrl")}/user`,
-            failOnStatusCode: false,
-            // headers: {
-            //     Authorization: `${Cypress.env("authorizationToken")}`
-            // }
-        }).then((response) => {
-            expect(response.status).to.eq(403)
-            expect(response.body).to.have.property('errors')
-            expect(response.body.errors).to.include('No token provided.')
-
-
-        })
-
-    })
-
     it('Alterar email', () => {
         cy.request({
             method: 'PUT',
